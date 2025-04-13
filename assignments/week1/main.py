140fc97a730f695a013fee2a67326502d3a2b601
c-valeria-mj <Claudia.v.jocol@stud.leuphana.de>
2025-04-13 22:35:48 +0200

    Assignment one completed

---
+++ assignments/week1/main.py
@@ -0,0 +1,18 @@
+# Assignment 1 - due 14.04.2025
+# Print ASCII art of choice
+
+# this list holds the multiple lines of strings that make up the ASCII art
+ascii_art = [
+    "|￣￣￣￣￣￣￣￣￣￣|",
+    "   Hello World!    ",
+    "   Programming     ",
+    "     is fun :)     ",
+    "|＿＿＿＿＿＿＿＿＿＿|",
+    "(\__/)  ||",
+    "(•ㅅ•)  ||",
+    "/     づ"
+]
+
+# iterate over list and print each line of the ASCII art
+for i in ascii_art:
+    print(i)
